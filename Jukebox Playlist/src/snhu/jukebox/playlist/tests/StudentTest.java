30ad2846ea1914fa96f1565ab0fc144342be1fc3
Cassidy <cassidy.yohn@snhu.edu>
2021-02-06 18:26:28 -0500

    Added a test case to retrieve my profile in StudentTest.java. - CY
    Change-Id: I0000000000000000000000000000000000000001

--- Jukebox Playlist/src/snhu/jukebox/playlist/tests/StudentTest.java
+++ Jukebox Playlist/src/snhu/jukebox/playlist/tests/StudentTest.java
@@ -32,6 +32,14 @@ public class StudentTest {
 	
 	//Module 5 - Add your unit test case here to check for your name after you have added it to the StudentList
 	
+	//adding unit test case to retrieve my profile - CY
+	@Test
+	public void testGetStudentNameList3() {
+		List<String> studentNames = new ArrayList<String>();							//create variable for student list of names
+		StudentList studentList = new StudentList();									//instantiate the StudentList object so we can access it's methods and properties
+		studentNames = studentList.getStudentsNames();									//populate the studentNames list with the actual values in the StudentsList object
+		assertEquals("Cassidy", studentNames.get(2));									//test case for pass/fail. We expect the first name to be Cassidy. 
+	}
 	
 	
 	
